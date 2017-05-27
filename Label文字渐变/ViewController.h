b0cb073888dbc0bc6b2198f419e40ef1ef294290
王涛 <1315455680@qq.com>
2017-05-27 10:52:49 +0800

    Initial Commit

---
+++ Label文字渐变/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Label文字渐变
+//
+//  Created by 王涛 on 2017/5/27.
+//  Copyright © 2017年 王涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Label文字渐变/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Label文字渐变
+//
+//  Created by 王涛 on 2017/5/27.
+//  Copyright © 2017年 王涛. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
